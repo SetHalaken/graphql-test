535ee47d5cc3fd39fa704629bf47538948e67d15
Alberto Sandoval <setHalaken@gmail.com>
2019-04-06 19:57:25 -0600

    all dependencies

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
-const express = require('express');
+import express from "express";
+import graphqlHTTP from "express-graphql"
 
 const app = express();
+
+const schema = {};
+
+app.use('/graphql', graphqlHTTP({
+    graphiql: true,
+    schema: schema
+}));
 
 app.listen(3000, () => console.log('Server on port 3000'));
